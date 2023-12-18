8b2c03d7c742a38fef25c8dd2db03d5bdbd3c636
Kyle <kylemcguire35@gmail.com>
2023-12-17 23:54:07 -0800

    added buttons to change date to view other reservations

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -12,10 +12,13 @@ import ReservationsList from "../layout/ReservationsList";
 function Dashboard({ date }) {
   const [reservations, setReservations] = useState([]);
   const [reservationsError, setReservationsError] = useState(null);
+  const [counter, setCounter] = useState(0);
 
-  useEffect(loadDashboard, [date]);
+  useEffect(() => {
+    loadDashboard(date);
+  }, [date]);
 
-  function loadDashboard() {
+  function loadDashboard(date) {
     const abortController = new AbortController();
     setReservationsError(null);
     listReservations({ date }, abortController.signal)
@@ -24,14 +27,44 @@ function Dashboard({ date }) {
     return () => abortController.abort();
   }
 
+  function adjustDate(button) {
+    setCounter((prevCounter) => {
+      let newCounter = prevCounter;
+
+      if (button === "today") {
+        newCounter = 0;
+      } else if (button === "next") {
+        newCounter += 1;
+      } else if (button === "prev") {
+        newCounter -= 1;
+      }
+
+      // Use the updated counter to calculate the new date
+      const currentDate = new Date(date);
+      currentDate.setDate(currentDate.getDate() + newCounter);
+      const displayDate = currentDate.toISOString().split("T")[0];
+
+      // Call loadDashboard with the updated date
+      console.log(displayDate);
+      loadDashboard(displayDate);
+
+      return newCounter;
+    });
+  }
+
   return (
     <main>
       <h1>Dashboard</h1>
       <div className="d-md-flex mb-3">
         <h4 className="mb-0">Reservations for date</h4>
       </div>
+      <div>
+        <button onClick={() => adjustDate("next")}>Next</button>
+        <button onClick={() => adjustDate("prev")}>Previous</button>
+        <button onClick={() => adjustDate("today")}>Today</button>
+      </div>
       <ErrorAlert error={reservationsError} />
-      <ReservationsList reservations={reservations}/>
+      <ReservationsList reservations={reservations} />
     </main>
   );
 }
