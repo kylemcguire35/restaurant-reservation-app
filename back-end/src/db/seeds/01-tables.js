625de7cec9cd55f044f040660bb94e5de4a5e7b4
Kyle <kylemcguire35@gmail.com>
2023-12-20 20:31:55 -0800

    created seed file for tables

--- back-end/src/db/seeds/01-tables.js
+++ back-end/src/db/seeds/01-tables.js
@@ -1,25 +1,24 @@
 exports.seed = function (knex) {
-    return knex("tables").insert([
-      {
-        table_name: "Bar #1",
-        free: true,
-        capacity: 1,
-      },
-      {
-        table_name: "Bar #2",
-        free: true,
-        capacity: 1,
-      },
-      {
-        table_name: "#1",
-        free: true,
-        capacity: 6,
-      },
-      {
-        table_name: "#2",
-        free: true,
-        capacity: 6,
-      },
-    ]);
-  };
-  
+  return knex("tables").insert([
+    {
+      table_name: "Bar #1",
+      capacity: 1,
+      free: true,
+    },
+    {
+      table_name: "Bar #2",
+      capacity: 1,
+      free: true,
+    },
+    {
+      table_name: "#1",
+      capacity: 6,
+      free: true,
+    },
+    {
+      table_name: "#2",
+      capacity: 6,
+      free: true,
+    },
+  ]);
+};
