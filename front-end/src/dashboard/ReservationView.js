1dbd655ba23f79331dd4f4b8b09daa0d02b0f517
Kyle <kylemcguire35@gmail.com>
2023-12-21 16:50:11 -0800

    add Seat button

--- front-end/src/dashboard/ReservationView.js
+++ front-end/src/dashboard/ReservationView.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function ReservationView({ reservation }) {
   return (
     <div>
-      <p>{reservation.first_name} {reservation.last_name}</p>
+      <p>
+        {reservation.first_name} {reservation.last_name}
+      </p>
       <p>{reservation.reservation_time}</p>
       <p>{reservation.people}</p>
       <p>{reservation.mobile_number}</p>
+      <Link to={`/reservations/${reservation.reservation_id}/seat`}>Seat</Link>
     </div>
   );
 }
